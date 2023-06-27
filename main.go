81ab4650b9f6bfbd1f05c63fb560750def204be2
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-06-27 18:22:39 +0530

    entry point

--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"net/http"
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-	"net/http"
-	"os"
+	"github.com/krishnanshagarwal112/rss-aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+	
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB : dbQueries,
+	}
 
 	router := chi.NewRouter()
 	v1Router := chi.NewRouter()
 
 	srv := http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: router,
 	}
 
@@ -33,7 +60,12 @@ func main() {
 	router.Mount("/v1", v1Router)
 
 	v1Router.Get("/healthz", handlerReadiness)
-	v1Router.Get("/err",err500)
+	v1Router.Get("/err", err500)
+
+	v1Router.Post("/users",func(w http.ResponseWriter, r *http.Request) {
+		userPost(w,r,&apiCfg)
+	})
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
