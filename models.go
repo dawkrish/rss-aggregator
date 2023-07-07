d8b690f9370a2fe6ef515b1b170ae3e8acbe4cd9
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-07-07 16:21:57 +0530

    models

--- models.go
+++ models.go
@@ -11,6 +11,16 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
+	ApiKey string `json:"api_key"`
+}
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url string `json:"url"`
+	UserID  uuid.UUID `json:"user_id"`
 }
 
 func databaseUserToUser(user database.User) User {
@@ -19,5 +29,17 @@ func databaseUserToUser(user database.User) User {
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 		Name:      user.Name,
+		ApiKey: user.Apikey,
+	}
+}
+
+func databaseFeedToFeed(feed database.Feed) Feed {
+	return Feed{
+		ID:        feed.ID,
+		CreatedAt: feed.CreatedAt,
+		UpdatedAt: feed.UpdatedAt,
+		Name:      feed.Name,
+		Url: feed.Url,
+		UserID: feed.UserID,
 	}
 }
